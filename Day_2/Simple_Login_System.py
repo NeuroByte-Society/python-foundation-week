ef62d23e7c5ad8a6167c5f8fea4131b6828f658e
Vishal Maurya <vis62556@gmail.com>
2025-07-30 20:35:31 +0530

    Create Simple_Login_System.py

---
+++ Day_2/Simple_Login_System.py
@@ -0,0 +1,11 @@
+# Preset password
+correct_password = "python123"
+
+# Take user input
+user_password = input("Enter your password: ")
+
+# Check
+if user_password == correct_password:
+    print("Login successful!")
+else:
+    print("Incorrect password. Try again.")
