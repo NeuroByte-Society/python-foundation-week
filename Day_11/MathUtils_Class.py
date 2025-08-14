c1a9c5f019e7ca9121ef873c6a97912963488f6b
Vishal Maurya <vis62556@gmail.com>
2025-08-14 20:00:41 +0530

    Create MathUtils_Class.py

---
+++ Day_11/MathUtils_Class.py
@@ -0,0 +1,16 @@
+class MathUtils:
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def subtract(a, b):
+        return a - b
+
+    @staticmethod
+    def multiply(a, b):
+        return a * b
+
+print(MathUtils.add(10, 5))       # 15
+print(MathUtils.subtract(10, 5))  # 5
+print(MathUtils.multiply(4, 3))   # 12
